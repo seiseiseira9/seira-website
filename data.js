909583441a95398d401a2dcdb02b851dfd81faf5
katorimiyu <seira@katorimiyunoMacBook-Pro.local>
2026-04-08 13:10:45 +0900

    Initial commit: 聖来のウェブサイト

---
+++ data.js
@@ -0,0 +1,23 @@
+const DATA = {
+  heroSub:    'みんなの元カノ',
+  heroName:   '南星 聖来',
+  heroNameEn: 'Nansei Seira',
+  heroCatch:  '気になっちゃいました？',
+  profile: {
+    name:   '南星 聖来（ナンセ セイラ）',
+    age:    '25',
+    height: '163 cm',
+    blood:  'A型',
+    shop:   'クラブブレンダ 難波店',
+    area:   '大阪・日本橋 / 東京にもよくいます',
+    bio:    'おはこんにちこんばんわ💞\nみんなの元カノやらせてもらってます。\nブレンダ難波の聖来です🪼💞\n東京にもよくいます。',
+  },
+  sns: [
+    { icon: '🏠', label: 'Blenda 難波',   desc: '予約・詳細はこちら', url: 'https://www.cityheaven.net/osaka/A2702/A270202/blenda/girlid-60924755/' },
+    { icon: '💬', label: 'Official LINE', desc: 'ID: mny9mny',       url: 'https://lin.ee/mny9mny' },
+    { icon: '𝕏',  label: 'X (Twitter)',  desc: '@seiseiseira__',     url: 'https://twitter.com/seiseiseira__' },
+    { icon: '▶',  label: 'YouTube',       desc: 'せいらんど',          url: 'https://youtube.com/@seiseiseira__?si=87b606QfmREKfH-g' },
+    { icon: '♪',  label: 'TikTok',        desc: 'みんなの元カノ',       url: 'https://www.tiktok.com/@seiseiseira__' },
+    { icon: '📷', label: 'Instagram',     desc: '@seiseiseira__',     url: 'https://www.instagram.com/seiseiseira__' },
+  ],
+};
